85ff355bfc1dc2c6592d4e089234e2132ab8f009
yoichi kobayashi <info@tplh.net>
2015-10-23 12:33:53 +0900

    [modfiy] texture部分を修正。

--- js/main.js
+++ js/main.js
@@ -13,6 +13,9 @@ var exports = function(){
   Background.prototype.init = function(scene) {
     this.video.muted = true;
     this.texture = new THREE.VideoTexture(this.video);
+    this.texture.minFilter = THREE.LinearFilter;
+    this.texture.magFilter = THREE.LinearFilter;
+    this.texture.format = THREE.RGBFormat;
     this.geometry = new THREE.SphereGeometry(this.r, this.segment, this.segment);
     this.geometry.applyMatrix(new THREE.Matrix4().makeScale(-1, 1, 1));
     this.material = new THREE.MeshBasicMaterial({
